a6f8bab18b6f2f272e37ea530b37f049c00209ee
Alexander Celis <dacelis0@misena.edu.co>
2021-08-17 08:04:23 -0500

    add: create slider

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+import Swiper from "https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js";
+
+const options = {
+  /* direction: "horizontal", */
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+};
+
+const swiper = new Swiper(".swiper-container", options);
